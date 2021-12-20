b15eb46871a366f7a370d249e04992a8d50b9943
kaiyuan <1409291878@qq.com>
2021-12-20 20:10:37 +0800

    coment for sort

--- classic-algorithm/Sort/sort1.c
+++ classic-algorithm/Sort/sort1.c
@@ -28,18 +28,19 @@ void InsertionSort(ElementType A[], int N)
 
 /* Shellsort */
 //
-//希尔 增量序列
+/* begin */
+/* 1. 希尔增量序列实现希尔排序 */
 void Shellsort(ElementType A[], int N)
 {
     int i, j, Increment;
     ElementType Tmp;
 
-    //排序间隔从 N/2 到 1
+    /* 排序间隔从 N/2 到 1 */
     for (Increment = N / 2; Increment > 0; Increment /= 2)
     {
+        /* 这里便是你熟悉的插入排序的思想 */
         for (i = Increment; i < N; i++)
         {
-            //插入的思想
             Tmp = A[i];
             for (j = i; j >= Increment; j -= Increment)
             {
@@ -56,15 +57,18 @@ void Shellsort(ElementType A[], int N)
         }
     }
 }
-//Sedgewick 增量序列
+/* 2. Sedgewick 增量序列实现希尔排序 */
 void Shellsort_Sedgewick(ElementType A[], int N)
 {
     int Si, i, j, Increment;
     ElementType Tmp;
 
+    /* 这里 Sedgewick 增量只列出了一部分 , 最大间隔只有 929 , 你应该根据你自己的需求添加 929 之后的间隔 */
+    /* 这里多一个 0 是为了给出一个排序截止信号 */
     int Sedgewick[] = {929, 505, 209, 109, 41, 19, 5, 1, 0};
     for (Si = 0; Sedgewick[Si] >= N; Si++)
     {
+        /* 这个循环截断多余的大的间隔，因为间隔的值起码不能大于待判定数组元素的个数吧 */
         ;
     }
 
@@ -81,15 +85,20 @@ void Shellsort_Sedgewick(ElementType A[], int N)
         }
     }
 }
+/* end */
 
 /* quicksort */
 //
+/* begin */
+/* 1. 三值的判断与分配 */
 /* Return median of Left, Center, and Right */
 /* Order these and hide the pivot */
 ElementType Median3(ElementType A[], int Left, int Right)
 {
     int Center = (Left + Right) / 2;
 
+    /* 前两个 if 之后 , 成功赋值 A[Left] 为最小 */
+    /* 最后一个 if 之后 , A[Right] 最大 , A[Center] 中是 Pivot */
     if (A[Left] > A[Center])
     {
         Swap(&A[Left], &A[Center]);
@@ -110,14 +119,17 @@ ElementType Median3(ElementType A[], int Left, int Right)
     return A[Right - 1]; /* Return pivot */
 }
 
-#define Cutoff (3) //截止
+/* 设置截止范围 */
+#define Cutoff (10)
 
+/* 2. 核心递归 */
+/* 为 Right - Left + 1 个元素排序 */
 void Qsort(ElementType A[], int Left, int Right)
 {
     int i, j;
     ElementType Pivot;
 
-    if (Left + Cutoff <= Right)
+    if (Left + Cutoff <= Right) /* Cutoff < Right - Left + 1 */
     {
         Pivot = Median3(A, Left, Right);
         i = Left;
@@ -125,13 +137,14 @@ void Qsort(ElementType A[], int Left, int Right)
 
         for (;;)
         {
+            /* 我们的分割策略是等于时也停止 */
             while (A[++i] < Pivot)
             {
-                //结束循环后 A[i] >= Pivot
+                /* 结束循环后 A[i] >= Pivot */
             }
             while (A[--j] > Pivot)
             {
-                //结束循环后 A[j] <= Pivot
+                /* 结束循环后 A[j] <= Pivot */
             }
             if (i < j)
             {
@@ -150,18 +163,20 @@ void Qsort(ElementType A[], int Left, int Right)
     }
     else /* Do an insertion sort on the subarray */
     {
-        InsertionSort(A + Left, Right - Left + 1); //Right - Left + 1 < 4 ?
+        InsertionSort(A + Left, Right - Left + 1);
     }
 }
 
+/* 3. 驱动程序 */
 void Quicksort(ElementType A[], int N)
 {
     Qsort(A, 0, N - 1);
 }
-/* END */
 
 /* This code doesn't work */
 /*
+    如果 A[i] = A[j] = Pivot , 那么就会陷入无限循环
+
     i = Left + 1;
     j = Right - 2;
     for (;;)
@@ -177,3 +192,4 @@ void Quicksort(ElementType A[], int N)
     }
 */
 
+/* end */
--- classic-algorithm/Sort/sort2.c
+++ classic-algorithm/Sort/sort2.c
@@ -34,7 +34,8 @@ void Bucketsort(int *unsorted, int N, int maxNumber)
 
 /* radixsort */
 //
-//辅助函数，求数据的最大位数
+/* begin */
+/* 1. 辅助函数，求数据的最大位数 */
 int maxbit(int data[], int n)
 {
     /* 先求出最大数 */
@@ -57,18 +58,21 @@ int maxbit(int data[], int n)
         }
         count++;
     }
+
     return count;
 }
 
+/* 2. 主程序 */
 void Radixsort(int data[], int n)
 {
     int i, j, k;
-    int d = maxbit(data, n);
-
     int *tmpdata = malloc(sizeof(int) * n);
     int *bucket = malloc(sizeof(int) * 10); //计数器
 
-    /* 进行 d 次排序 */
+    int d = maxbit(data, n);
+
+    /* 最大元素有 d 位 , 进行 d 次排序 */
+    /* i = x 的循环结束后 , 元素的第 x 位被成功排序 */
     int radix = 1;
     for (i = 1; i <= d; i++)
     {
@@ -78,21 +82,23 @@ void Radixsort(int data[], int n)
             bucket[j] = 0;
         }
 
-        /* 统计每个桶中的记录数 */
+        /* 把数分配到桶中*/
         for (j = 0; j < n; j++)
         {
-            /* k 为 radix 位上的数 */
+            /* k 为 data[j] 从右往左第 i 位上的数 */
             k = (data[j] / radix) % 10;
             bucket[k]++;
         }
 
-        /* 记录位置 , 为下个循环的分配做铺垫 */
+        /* 记录第 x 位相同的元素在第 x 位排序后 , 这类元素在数组的末位置 */
+        /* 为下面循环的分配做铺垫 */
+        /* buket[9] 在循环之后总是为 n */
         for (j = 1; j < 10; j++)
         {
             bucket[j] = bucket[j - 1] + bucket[j];
         }
 
-        /* 将所有桶中记录依次收集到 tmpdata 中 */
+        /* 依桶中的信息为数据排序并存储到 tmpdata 中 */
         for (j = n - 1; j >= 0; j--)
         {
             /* k 为 radix 位上的数 */
@@ -113,10 +119,13 @@ void Radixsort(int data[], int n)
     free(tmpdata);
     free(bucket);
 }
+/* end */
 
 /* heapsort */
 //
-//从数组下标 0 开始存储
+/* begin */
+/* 从数组下标 0 开始存储 */
+/* 所以一个节点的左儿子自然就是这个表达式，读者自行验证 */
 #define LeftChild(i) (2 * (i) + 1)
 
 void Swap(int *x, int *y)
@@ -127,7 +136,7 @@ void Swap(int *x, int *y)
     *y = temp;
 }
 
-// N 个元素 , 第 i+1 个节点下降 ( 数组中的元素 A[i] )
+/* 在有 N 个节点的堆中 , 下滤节点 A[i] */
 void PercDown(ElementType A[], int i, int N)
 {
     int Child;
@@ -138,6 +147,7 @@ void PercDown(ElementType A[], int i, int N)
         Child = LeftChild(i);
         if (Child != N - 1 && A[Child + 1] > A[Child])
         {
+            /* 如果 i 有左儿子且大于右儿子，那么从右儿子的方向下滤 */
             Child++;
         }
         if (Tmp < A[Child])
@@ -157,36 +167,47 @@ void Heapsort(ElementType A[], int N)
     int i;
 
     /* BuildHeap */
+    /* 至于它的思想已在堆那一章分析过了 */
     for (i = N / 2; i >= 0; i--)
     {
         PercDown(A, i, N);
     }
 
+    /* DeleteMax */
     for (i = N - 1; i > 0; i--)
     {
         Swap(&A[0], &A[i]); /* DeleteMax */
         PercDown(A, 0, i);
     }
 }
+/* end */
 
 /* mergesort */
 //
+/* begin */
+/* 1. 合并两个数组 */
+/*
+    Lpos LeftEnd
+    Rpos RightEnd
+*/
+/* 分别记录 */
+/* 第一个数组的起始和终点 */
+/* 第二个数组的起始和终点 */
 void Merge(ElementType A[], ElementType TmpArray[],
            int Lpos, int Rpos, int RightEnd)
 {
-    //Lpos LeftEnd
-    //Rpos RightEnd
 
     int LeftEnd;
     LeftEnd = Rpos - 1;
 
-    // NumElements 本次排序的元素总个数
-    // TmpPos 临时数组当前的填充位置
+    /* NumElements 本次排序的元素总个数 */
+    /* TmpPos 临时数组当前的填充位置 */
     int i, NumElements, TmpPos;
     TmpPos = Lpos;
     NumElements = RightEnd - Lpos + 1;
 
     /* main loop */
+    /* 循环结束，要么 Lpos > LeftEnd，要么 Rpos > RightEnd */
     while (Lpos <= LeftEnd && Rpos <= RightEnd)
     {
         if (A[Lpos] <= A[Rpos])
@@ -215,6 +236,7 @@ void Merge(ElementType A[], ElementType TmpArray[],
     }
 }
 
+/* 2. */
 void MSort(ElementType A[], ElementType TmpArray[],
            int Left, int Right)
 {
@@ -229,6 +251,7 @@ void MSort(ElementType A[], ElementType TmpArray[],
     }
 }
 
+/* 3. 驱动程序 */
 void Mergesort(ElementType A[], int N)
 {
     ElementType *TmpArray;
@@ -240,3 +263,4 @@ void Mergesort(ElementType A[], int N)
         free(TmpArray);
     }
 }
+/* end */
